0338ae3ed38a875dd93b7f9023d285daf90add9b
Buterflyyy <125492683+Buterflyyy@users.noreply.github.com>
2024-11-07 15:40:32 -0600

    fukrron

--- negocio/src/app/routes.php
+++ negocio/src/app/routes.php
@@ -3,7 +3,7 @@ namespace App\controllers;
 use Slim\Routing\RouteCollectorProxy;
 
 /* Hola Prueba ;v
-/*fuckarron supervisado :PP
+/*fuckarron supervisado ali prueba :PP
 /* require __DIR__ . '/../controllers/Cliente.php';
 $app->group('/api',function(RouteCollectorProxy $api){
     $api->group('/cliente',function(RouteCollectorProxy $cliente){
